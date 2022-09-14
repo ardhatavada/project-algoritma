16c1f57116a30db3581d7f6baed23e66afea823b
ardhatavada <113041717+ardhatavada@users.noreply.github.com>
2022-09-14 23:21:28 +0700

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,16 @@
+'''In a file called bank.py, implement a program that prompts 
+the user for a greeting. If the greeting starts with “hello”, 
+output $0. If the greeting starts with an “h” (but not “hello”), 
+output $20. Otherwise, output $100. Ignore any leading whitespace 
+in the user’s greeting, and treat the user’s greeting case-insensitively.'''
+
+
+
+x = input("Greeting? ").strip().lower()
+
+if "hello" in x:
+    print("$0")
+elif "h" == x[0]:
+    print("$20")
+else:
+    print("$100")
